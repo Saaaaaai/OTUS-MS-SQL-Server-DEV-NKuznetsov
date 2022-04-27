055f6bc45f224dae579ee9e61ce5b328ecaf1d1c
Nikita Kuznetsov <happii15@yandex.ru>
2022-04-27 22:01:52 +0300

    Update hw_dml_tasks-188-4151ba.sql

--- HW09 - dml/hw_dml_tasks-188-4151ba.sql
+++ HW09 - dml/hw_dml_tasks-188-4151ba.sql
@@ -66,10 +66,76 @@ CustomerID = (select top 1 CustomerID
 4. Написать MERGE, который вставит вставит запись в клиенты, если ее там нет, и изменит если она уже есть
 */
 
-напишите здесь свое решение
+MERGE sales.customers as TARGET
+using (
+select top 5
+customerID + 10000, CustomerName+' NEW MERGE', BillToCustomerID, CustomerCategoryID, BuyingGroupID, PrimaryContactPersonID, AlternateContactPersonID,
+DeliveryCityID, PostalCityID, CreditLimit, AccountOpenedDate, StandardDiscountPercentage, IsStatementSent, IsOnCreditHold,
+PaymentDays, PhoneNumber, FaxNumber, DeliveryRun, RunPosition, WebsiteURL, DeliveryAddressLine1, DeliveryAddressLine2,
+DeliveryPostalCode, DeliveryLocation, PostalAddressLine1, PostalAddressLine2, PostalPostalCode, LastEditedBy, DeliveryMethodID
+from sales.Customers)
+AS SOURCE (customerID,CustomerName, BillToCustomerID, CustomerCategoryID, BuyingGroupID, PrimaryContactPersonID, AlternateContactPersonID,
+DeliveryCityID, PostalCityID, CreditLimit, AccountOpenedDate, StandardDiscountPercentage, IsStatementSent, IsOnCreditHold,
+PaymentDays, PhoneNumber, FaxNumber, DeliveryRun, RunPosition, WebsiteURL, DeliveryAddressLine1, DeliveryAddressLine2,
+DeliveryPostalCode, DeliveryLocation, PostalAddressLine1, PostalAddressLine2, PostalPostalCode, LastEditedBy, DeliveryMethodID)
+ON (target.customerid = source.customerid)
+when matched
+	then update set customerName = target.CustomerName + 'Updated MERGE'
+when not matched
+	then insert (CustomerName, BillToCustomerID, CustomerCategoryID, BuyingGroupID, PrimaryContactPersonID, AlternateContactPersonID,
+DeliveryCityID, PostalCityID, CreditLimit, AccountOpenedDate, StandardDiscountPercentage, IsStatementSent, IsOnCreditHold,
+PaymentDays, PhoneNumber, FaxNumber, DeliveryRun, RunPosition, WebsiteURL, DeliveryAddressLine1, DeliveryAddressLine2,
+DeliveryPostalCode, DeliveryLocation, PostalAddressLine1, PostalAddressLine2, PostalPostalCode, LastEditedBy, DeliveryMethodID)
+	values (source.CustomerName, source.BillToCustomerID, source.CustomerCategoryID, source.BuyingGroupID, 
+	source.PrimaryContactPersonID, source.AlternateContactPersonID, source.DeliveryCityID, 
+	source.PostalCityID, source.CreditLimit, source.AccountOpenedDate, source.StandardDiscountPercentage, 
+	source.IsStatementSent, source.IsOnCreditHold, source.PaymentDays, source.PhoneNumber, source.FaxNumber, 
+	source.DeliveryRun, source.RunPosition, source.WebsiteURL, source.DeliveryAddressLine1, source.DeliveryAddressLine2,
+	source.DeliveryPostalCode, source.DeliveryLocation, source.PostalAddressLine1, source.PostalAddressLine2, 
+	source.PostalPostalCode, source.LastEditedBy, source.DeliveryMethodID)
+output inserted.*, $action;
+
 
 /*
 5. Напишите запрос, который выгрузит данные через bcp out и загрузить через bulk insert
 */
 
-напишите здесь свое решение
+select @@SERVERNAME
+
+exec sp_configure 'show advanced options', 1;
+go
+
+reconfigure;
+go
+
+exec sp_configure 'xp_cmdshell',1;
+go
+
+reconfigure;
+go
+
+exec..xp_cmdshell 'bcp "[WideWorldImporters].Sales.Customers" out "D:\bcp.txt" -T -w -t";" -S DESKTOP-6182RB5\SQL2017';
+
+
+select * into new_bulk_tab from Sales.Customers
+where 1=2;
+
+select * from new_bulk_tab;
+
+BULK INSERT [new_bulk_tab]
+			from "D:\bcp.txt"
+			with
+			(
+				BATCHSIZE = 1000, 
+				DATAFILETYPE = 'widechar',
+				FIELDTERMINATOR = ';',
+				ROWTERMINATOR ='\n',
+				KEEPNULLS,
+				TABLOCK    
+			);
+
+select * from new_bulk_tab;
+
+
+
+
